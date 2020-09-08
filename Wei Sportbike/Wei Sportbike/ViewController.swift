0aa6dd07f6f363002a8a931dddd6db1e9326c398
Wei Li <wl2451@columbia.edu>
2020-09-07 19:21:14 -0700

    added background, business name, and contact information

---
+++ Wei Sportbike/Wei Sportbike/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Wei Sportbike
+//
+//  Created by Wei Li on 9/1/20.
+//  Copyright © 2020 CMU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
